68035a76a4d49a1a4953e9163d8d5dea2213cc98
Alex Bell <alexdbell@outlook.com>
2024-12-07 22:39:10 -0500

    Add dags/s3_snow.py

--- dags/s3_snow.py
+++ dags/s3_snow.py
@@ -1,11 +1,14 @@
 
+from airflow import DAG
+from datetime import datetime
+
 try:
     from airflow.providers.amazon.aws.transfers.s3_to_snowflake import S3ToSnowflakeOperator
 except ImportError:
-    from airflow.providers.amazon.aws.operators.s3_to_snowflake import S3ToSnowflakeOperator
-
-from airflow import DAG
-from datetime import datetime
+    try:
+        from airflow.providers.amazon.aws.operators.s3_to_snowflake import S3ToSnowflakeOperator
+    except ImportError as e:
+        raise ImportError(f"Failed to import S3ToSnowflakeOperator: {e}")
 
 default_args = {
     "owner": "airflow",
@@ -18,18 +21,18 @@ default_args = {
 with DAG(
     dag_id="s3_snow",
     default_args=default_args,
+    description="DAG to transfer data from S3 to Snowflake",
     schedule_interval="@daily",
     start_date=datetime(2024, 12, 8),
     catchup=False,
 ) as dag:
-
     s3_to_snowflake = S3ToSnowflakeOperator(
         task_id="s3_to_snowflake",
         s3_bucket="scarfdata",
         s3_key="",
         aws_conn_id="aws_default",
         snowflake_conn_id="snowflake_default",
-        stage="s3_snow_stage",
+        stage="public.s3_snow_stage",
         file_format="(type=csv, field_delimiter=',', skip_header=1)",
     )
     
