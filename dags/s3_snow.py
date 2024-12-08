3c9f88c3292c473dd8ae2b07a2ca9dfaae6af1db
Alex Bell <alexdbell@outlook.com>
2024-12-07 20:53:51 -0500

    Add dags/s3_snow.py

--- dags/s3_snow.py
+++ dags/s3_snow.py
@@ -1,7 +1,6 @@
 
 from airflow import DAG
 from airflow.providers.amazon.aws.transfers.s3_to_snowflake import S3ToSnowflakeOperator
-from airflow.models import Variable
 from datetime import datetime
 
 default_args = {
@@ -16,17 +15,23 @@ with DAG(
     dag_id="s3_snow",
     default_args=default_args,
     description="DAG to transfer data from S3 to Snowflake",
-    schedule_interval="@daily",
+    schedule_interval="",
     start_date=datetime(2024, 12, 8),
     catchup=False,
 ) as dag:
+
     s3_to_snowflake = S3ToSnowflakeOperator(
         task_id="s3_to_snowflake",
         s3_bucket="scarfdata",
         s3_key="",
         aws_conn_id="aws_default",
-        snowflake_conn_id="snowflake_default",
-        stage="public.s3_snow_stage",
+        snowflake_conn_id=None,
+        account="ia75458.rsggkwv.snowflakecomputing.com",
+        warehouse="compute_wh",
+        database="TEST_DB",
+        schema="public",
+        user="juliogeordio1",
+        password="Newcastle1!",
         file_format="(type=csv, field_delimiter=',', skip_header=1)",
     )
     
