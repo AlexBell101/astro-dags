a1af880ed33211273fb08b97c2d348ee66d4d19c
Alex Bell <alexdbell@outlook.com>
2024-12-09 13:33:26 -0500

    Update booty.py

--- dags/booty.py
+++ dags/booty.py
@@ -1,4 +1,3 @@
-
 from airflow import DAG
 from airflow.providers.snowflake.transfers.copy_into_snowflake import CopyFromExternalStageToSnowflakeOperator
 from datetime import datetime
@@ -12,22 +11,22 @@ default_args = {
 }
 
 with DAG(
-    dag_id="booty",
+    dag_id="boot",
     default_args=default_args,
     schedule_interval="Hourly",
     start_date=datetime(2024, 12, 9),
     catchup=False,
 ) as dag:
 
+    # Task: Load Data into Snowflake
     load_to_snowflake = CopyFromExternalStageToSnowflakeOperator(
         task_id="load_s3_to_snowflake",
-        table="scarf",
-        schema="public",
+        table="scarf",  # Ensure this table exists
         database="TEST_DB",
+        schema="public",
         file_format="(TYPE = CSV, FIELD_DELIMITER = ',', SKIP_HEADER = 1)",
-        pattern=".*\.csv",
+        pattern=".*\\.csv",
         snowflake_conn_id="snowflake",
-        s3_key="Scarf/company-rollups-scarf-export-2024-08-05-2024-09-05.csv",
+        s3_key="Scarf/company-rollups-scarf-export-2024-08-05-2024-09-05.csv",  # File path in S3
         aws_conn_id="s3",
     )
-    
